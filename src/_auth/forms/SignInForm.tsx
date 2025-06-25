c86859f4a71a559a209703b78213fc0d714461d1
Satyam-pandey677 <satyampandey0241@gmail.com>
2025-06-25 17:24:59 +0530

    fix: Remove unnecessary console log for user loading state in SignInForm component

--- src/_auth/forms/SignInForm.tsx
+++ src/_auth/forms/SignInForm.tsx
@@ -20,7 +20,6 @@ const SignUpForm = () => {
   const { toast } = useToast()
   const {mutateAsync: createSessionLogin } = useSignInAccountMutation()
   const navigate = useNavigate()
-  console.log(isUserLoading)
 
   const form = useForm<z.infer<typeof SignInValidation>>({
     resolver: zodResolver(SignInValidation),
